8f9b4e35b438a890835832eecc4fdf3e1a38280b
harijoel <harijoel09@gmail.com>
2024-02-19 22:53:44 -0600

    TasnkInput basic HTML structured

--- client/src/components/TaskInput.vue
+++ client/src/components/TaskInput.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="task-input">
-    <!--
-      Mission One: Add a task input
-
-      Create a task input according to the design in the README.md file.
-
-      Definition of done:
-      [ ] User can type into the input
-      [ ] A button with the text "Add" is visible
-      [ ] User can click the button to submit the input
-       
-      Your submission will be judged out of ten points based
-      on the following criteria:
-      
-      Design fidelity - 5 points total
-        - Is the spacing correct? (e.g. padding, margin) - 2 points
-        - Is there good interaction feedback? (e.g. hover, focus, active states) - 3 points
-      Code quality - 5 points
-        - Are concerns clearly separeted? (e.g. clear responsibilities for functions, etc.) - 3 point
-        - Is the code easy to read and understand? - 2 points
-    -->
+    <div class="task-input">
+    <form >
+      <input placeholder="Add a new task...">
+      <button class="add-button">ADD</button>
+    </form>
+  </div>
   </div>
 </template>
 
